511b36c04bf5b22e14e26efca406bc9782128d22
Jim Stewart <jim.stewart@thelangschool.net>
2025-11-06 13:52:56 -0500

    set up main class to start

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
 from Sprite import *
+from Screen import *
+
+world_width = 10000
+world_height = 10000
+screen = Screen(4000,4000, 800, 600)
+
+# 1) read the sprites.txt file into a list
+# 2) create a list called sprites
+# 3) convert each line of the sprites.txt file into a Sprite object
+#    and add it to the sprites list
+# 4) make a list called rendered
+# 5) add each sprite that is inside the screen's boundaries to rendered
+# 6) call the screen.render() method on each rendered sprite at the x
+#    and y coordinate they'd be on the screen
--- Screen.py
+++ Screen.py
@@ -1,4 +1,9 @@
 class Screen:
-    def __init__(self, width, height):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
+
+    def render(self, sprite, x, y):
+        print(f"rendering a {sprite.type} sprite at x:{x}, y:{y}")
